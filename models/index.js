1fea45d80c460555d7c7b13aebf943f1f00b9028
BuildTools <unconfigured@null.spigotmc.org>
2023-11-18 16:18:41 -0700

    Sequalize Associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category)
 // Categories have many Products
-
+Category.hasMany(Product)
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Tag, { through: ProductTag})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag})
 
 module.exports = {
   Product,
