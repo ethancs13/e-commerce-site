f25bfb5f5db0d3ff0e3f7e2230880763e7c4e387
BuildTools <unconfigured@null.spigotmc.org>
2023-11-18 16:15:14 -0700

    query tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,32 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+CREATE TABLE Category (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    category_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL DEFAULT 10,
+    category_id INT,
+    FOREIGN KEY (category_id)
+    REFERENCES Category(id)
+)
+
+CREATE TABLE Tag (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE ProductTag (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY (product_id)
+    REFERENCES Product(id),
+    FOREIGN KEY (tag_id)
+    REFERENCES Tag(id)
+)
