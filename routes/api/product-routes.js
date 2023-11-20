27fd58f177549b7c7a0e11f9a5f06bd1518dc271
BuildTools <unconfigured@null.spigotmc.org>
2023-11-19 21:11:41 -0700

    include category and tag for model queries

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,9 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all products
   try {
-    const productData = await Product.findAll();
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -19,7 +21,9 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   try {
-    const product = await Product.findByPk(req.params.id);
+    const product = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
     if (!product) {
       res.status(404).json({ message: 'No product with this id!' });
       return;
